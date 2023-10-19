657a12aa60d03237fcab7e93e502f0a6b1b66e66
tanerjn <stanermetin@gmail.com>
2023-10-19 13:05:15 +0200

    corrected naming for each demo

--- 14-CPU_Memory_SYN_Count_Based_LB/c7-03-web-linux-vmss-resource.tf
+++ 14-CPU_Memory_SYN_Count_Based_LB/c7-03-web-linux-vmss-resource.tf
@@ -10,10 +10,10 @@ sudo systemctl start httpd
 sudo systemctl stop firewalld
 sudo systemctl disable firewalld
 sudo chmod -R 777 /var/www/html 
-sudo echo "Welcome to stacksimplify - WebVM App1 - VM Hostname: $(hostname)" > /var/www/html/index.html
+sudo echo "Welcome to LB with metrics Demo - WebVM App1 - VM Hostname: $(hostname)" > /var/www/html/index.html
 sudo mkdir /var/www/html/webvm
-sudo echo "Welcome to stacksimplify - WebVM App1 - VM Hostname: $(hostname)" > /var/www/html/webvm/hostname.html
-sudo echo "Welcome to stacksimplify - WebVM App1 - App Status Page" > /var/www/html/webvm/status.html
+sudo echo "Welcome to LB with metrics Demo - WebVM App1 - VM Hostname: $(hostname)" > /var/www/html/webvm/hostname.html
+sudo echo "Welcome to LB with metrics Demo - WebVM App1 - App Status Page" > /var/www/html/webvm/status.html
 sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(250, 210, 210);"> <h1>Welcome to Stack Simplify - WebVM APP-1 </h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>' | sudo tee /var/www/html/webvm/index.html
 sudo curl -H "Metadata:true" --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2020-09-01" -o /var/www/html/webvm/metadata.html
 sudo sh -c 'echo -e "[azure-cli] 
