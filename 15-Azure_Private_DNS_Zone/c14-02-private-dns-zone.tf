5889194b438907058ba6df48515a95b80138d48e
tanerjn <stanermetin@gmail.com>
2023-10-20 14:20:01 +0200

    modify DNS zone configuration filwith the correct domain

--- 15-Azure_Private_DNS_Zone/c14-02-private-dns-zone.tf
+++ 15-Azure_Private_DNS_Zone/c14-02-private-dns-zone.tf
@@ -1,6 +1,6 @@
 # Resource-1: Create Azure Private DNS Zone
 resource "azurerm_private_dns_zone" "private_dns_zone" {
-  name = "5gcore.de"
+  name = "terraformguru.com"
   resource_group_name = azurerm_resource_group.rg.name 
 }
 
