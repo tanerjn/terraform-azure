c3f81cf7d393976bd6778b2149d17578b8eaf6b6
tanerjn <stanermetin@gmail.com>
2023-10-26 15:05:21 +0200

    file restructuring

--- 18-Traffic-Manager/c8-05-bastion-outputs.tf
+++
@@ -1,7 +0,0 @@
-/*
-## Bastion Host Public IP Output
-output "bastion_host_linuxvm_public_ip_address" {
-  description = "Bastion Host Linux VM Public Address"
-  value = azurerm_public_ip.bastion_host_publicip.ip_address
-}
-*/
--- 18-Traffic-Manager/c9-01-web-loadbalancer-input-variables.tf
+++
@@ -1,1 +0,0 @@
-# Placeholder file for Load Balancer Input Variables
