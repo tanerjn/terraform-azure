3440d2aee1169495bc223e788f238b2b39c9a130
tanerjn <stanermetin@gmail.com>
2023-10-26 15:05:03 +0200

    added scaleset2 for Traffic Manager demo

---
+++ 18-Traffic-Manager/scaleset2/c1-versions.tf
@@ -0,0 +1,32 @@
+# Terraform Block
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.0" 
+    }
+    random = {
+      source = "hashicorp/random"
+      version = ">= 3.0"
+    }
+    null = {
+      source = "hashicorp/null"
+      version = ">= 3.0"
+    }    
+  }
+# Terraform State Storage to Azure Storage Container
+  backend "azurerm" {
+    resource_group_name   = "resgr-smiling-lobster"
+    storage_account_name  = "tfstateterrademo"
+    container_name        = "tfstatecontainer"
+    key                   = "project-1-eastus2-terraform.tfstate"
+  }    
+}
+
+# Provider Block
+provider "azurerm" {
+ features {}          
+}
+
+
