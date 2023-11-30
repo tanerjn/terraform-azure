5b76a090f1d8f19f2448dbaf79af5a1a7a904b55
tanerjn <stanermetin@gmail.com>
2023-11-30 09:36:00 +0100

    remove backend address pools

--- 19-Application-Gateway/multisite_hosting/terraform-manifests/c7-03-web-linux-vmss-app1-resource.tf
+++ 19-Application-Gateway/multisite_hosting/terraform-manifests/c7-03-web-linux-vmss-app1-resource.tf
@@ -57,7 +57,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "app1_web_vmss" {
       primary   = true
       subnet_id = azurerm_subnet.websubnet.id  
       #load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.web_lb_backend_address_pool.id]
-      application_gateway_backend_address_pool_ids = [azurerm_application_gateway.web_ag.backend_address_pool[0].id]            
+      #application_gateway_backend_address_pool_ids = [azurerm_application_gateway.web_ag.backend_address_pool[0].id]            
     }
   }
   #custom_data = filebase64("${path.module}/app-scripts/redhat-app1-script.sh")      
